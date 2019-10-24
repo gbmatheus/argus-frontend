3ebd278b62a27c5d996a4e7f1186a2ba35495959
gbmatheus <gabrielmatheus2503@gmail.com>
2019-10-24 03:28:54 -0300

    Adicionando tela sobre

---
+++ src/views/frontend/about/Index.vue
@@ -0,0 +1,31 @@
+<template>
+  <base-section
+    id="about"
+    fill-height
+  >
+    <page-jumbotron
+      :icon="mdiInformation"
+      heading="Sobre"
+      view="src/views/frontend/about/Index.vue"
+    />
+  </base-section>
+</template>
+
+<script>
+  // Icons
+  import {
+    mdiInformation,
+  } from '@mdi/js'
+
+  export default {
+    name: 'FrontendAbout',
+
+    components: {
+      PageJumbotron: () => import('@/components/PageJumbotron'),
+    },
+
+    data: () => ({
+      mdiInformation,
+    }),
+  }
+</script>
