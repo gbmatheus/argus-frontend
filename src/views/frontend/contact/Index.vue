af2bfac3553be2a6909e40cc6c1dbf062431ae39
gbmatheus <gabrielmatheus2503@gmail.com>
2019-10-24 03:29:05 -0300

    Adicionando tela contato

---
+++ src/views/frontend/contact/Index.vue
@@ -0,0 +1,31 @@
+<template>
+  <base-section
+    id="contact"
+    fill-height
+  >
+    <page-jumbotron
+      :icon="mdiContacts"
+      heading="Contatos"
+      view="src/views/frontend/contact/Index.vue"
+    />
+  </base-section>
+</template>
+
+<script>
+  // Icons
+  import {
+    mdiContacts,
+  } from '@mdi/js'
+
+  export default {
+    name: 'FrontendContact',
+
+    components: {
+      PageJumbotron: () => import('@/components/PageJumbotron'),
+    },
+
+    data: () => ({
+      mdiContacts,
+    }),
+  }
+</script>
