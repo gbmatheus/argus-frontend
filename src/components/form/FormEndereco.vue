8a6aa3c9f96274615e8b45b779d84c82d84a8569
gbmatheus <gabrielmatheus2503@gmail.com>
2019-11-07 02:58:11 -0300

    Termino formulário de endereço, mas sem componetização

--- src/components/form/FormEndereco.vue
+++ src/components/form/FormEndereco.vue
@@ -1,117 +1,127 @@
 <template>
-    <v-card
-      class="mx-auto"
-      max-width="800"
-      outlined
-    >
-      <v-card-title class="red darken-4 white--text">
-        Endereço
-      </v-card-title>
-      <v-container>
-        <v-row>
-          <v-col
-            cols="12"
-            md="9"
-          >
-            <v-text-field
-              v-model="rua"
-              :rules="nameRules"
-              :counter="50"
-              label="Rua"
-              filled
-              required
-            />
-          </v-col>
+  <v-card
+    class="mx-auto"
+    max-width="800"
+    outlined
+  >
+    <v-card-title class="red darken-4 white--text">
+      Endereço
+    </v-card-title>
+    <v-container>
+      <v-row>
+        <v-col
+          cols="12"
+          sm="10"
+          md="8"
+        >
+          <v-text-field
+            v-model="rua"
+            ref="rua"
+            :rules="nomeRules"
+            :counter="50"
+            label="Rua"
+            filled
+            required
+          />
+        </v-col>
 
-          <v-col
-            cols="12"
-            md="3"
-          >
-            <v-text-field
-              filled
-              v-model.number="numero"
-              :rules="numeroRules"
-              label="Nº"
-              required
-            />
-          </v-col>
-        </v-row>
+        <v-col
+          cols="12"
+          sm="6"
+          md="4"
+        >
+          <v-text-field
+            v-model.number="numero"
+            v-mask="maskNumero"
+            ref="numero"
+            :rules="numeroRules"
+            label="Nº"
+            filled
+            required
+          />
+        </v-col>
 
-        <v-row>
-          <v-col
-            cols="12"
-            md="7"
-          >
-            <v-text-field
-              filled
-              v-model="complemento"
-              :rules="opcionalRules"
-              :counter="50"
-              label="Complemento"
-            />
-          </v-col>
+        <v-col
+          cols="12"
+          sm="9"
+          md="6"
+        >
+          <v-text-field
+            v-model="complemento"
+            ref="complmento"
+            :rules="opcionalRules"
+            label="Complemento"
+            filled
+          />
+        </v-col>
 
-          <v-col
-            cols="12"
-            md="5"
-          >
-            <v-text-field
-              filled
-              v-model="bairro"
-              :rules="nomePequenoRules"
-              :counter="30"
-              label="Bairro"
-              required
-            />
-          </v-col>
-        </v-row>
+        <v-col
+          cols="12"
+          sm="6"
+          md="6"
+        >
+          <v-text-field
+            v-model="bairro"
+            ref="bairro"
+            :rules="nomePequenoRules"
+            :counter="30"
+            label="Bairro"
+            filled
+            required
+          />
+        </v-col>
 
-        <v-row>
-          <v-col
-            cols="12"
-            md="6"
-          >
-            <v-text-field
-              filled
-              v-model="cidade"
-              :rules="nomePequenoRules"
-              :counter="30"
-              label="Cidade"
-              required
-            />
-          </v-col>
+        <v-col
+          cols="12"
+          sm="6"
+          md="6"
+        >
+          <v-text-field
+            v-model="cidade"
+            ref="cidade"
+            :rules="nomePequenoRules"
+            :counter="30"
+            label="Cidade"
+            filled
+            required
+          />
+        </v-col>
 
-          <v-col
-            cols="12"
-            md="2"
-          >
-            <v-select
-              filled
-              v-model="uf"
-              :items="ufs"
-              :rules="[v => !!v || 'Item is required']"
-              label="UF"
-              required
-            />
-          </v-col>
+        <v-col
+          cols="12"
+          sm="4"
+          md="2"
+        >
+          <v-select
+            v-model="uf"
+            ref="uf"
+            :items="ufs"
+            :rules="[v => !!v || 'UF é obrigatório']"
+            label="UF"
+            filled
+            required
+          />
+        </v-col>
 
-          <v-col
-            cols="12"
-            md="4"
-          >
-            <v-text-field
-              filled
-              v-model.number="cep"
-              v-mask="maskCep"
-              :rules="cepRules"
-              label="CEP"
-              required
-            />
-          </v-col>
-        </v-row>
+        <v-col
+          cols="12"
+          sm="6"
+          md="4"
+        >
+          <v-text-field
+            v-model.number="cep"
+            v-mask="maskCep"
+            ref="cep"
+            :rules="cepRules"
+            label="CEP"
+            filled
+            required
+          />
+        </v-col>
+      </v-row>
 
-      </v-container>
-    </v-card>
+    </v-container>
+  </v-card>
 </template>
 
 <script>
@@ -126,6 +136,7 @@
       // valid: false,
       rua: '',
       numero: '',
+      maskNumero: '###############',
       complemento: '',
       bairro: '',
       cidade: '',
@@ -133,30 +144,53 @@
       maskCep: '##.###-###',
       uf: null,
       //  Regras de validação
-      nameRules: [
-        v => !!v || 'Name is required',
-        v => v.length <= 10 || 'Name must be less than 10 characters',
+      nomeRules: [
+        v => !!v || 'Obrigatório',
+        v => v.length <= 50 || 'Deve ter menos de 50 caracteres',
       ],
       numeroRules: [
-        v => !!v || 'Número é obrigatório',
-        v => /[0-9]/.test(v) || 'Número inválido',
+        v => !!v || 'Obrigatório',
+        // v => /[0-9]/.test(v) || 'Número inválido',
       ],
       nomePequenoRules: [
-        v => !!v || 'Name is required',
-        v => v.length <= 30 || 'Name must be less than 10 characters',
+        v => !!v || 'Obrigatório',
+        v => v.length <= 30 || 'Deve ter menos de 50 caracteres',
       ],
       opcionalRules: [
-        v => v.length <= 50 || 'Name must be less than 10 characters',
+        v => v.length <= 50 || 'Deve ter menos de 50 caracteres',
       ],
       cepRules: [
-        v => !!v || 'Name is required',
-        v => v.length <= 14 || 'Name must be less than 10 characters',
+        v => !!v || 'Obrigatório',
+        v => v.length <= 10 || 'Deve ter menos de 10 caracteres',
       ],
       ufs: [
+        'AC',
+        'AL',
+        'AP',
         'AM',
         'BA',
         'CE',
+        'DF',
+        'ES',
+        'GO',
+        'MA',
+        'MT',
+        'MS',
+        'MG',
+        'PA',
+        'PB',
+        'PR',
         'PE',
+        'PI',
+        'RJ',
+        'RN',
+        'RS',
+        'RO',
+        'RR',
+        'SC',
+        'SP',
+        'SE',
+        'TO',
       ],
     }),
   }
